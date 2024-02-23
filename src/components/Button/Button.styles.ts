a750167f52da724b87e333dfe4e215ba60a0d838
ATK3Shape <Alexandr.Tkachenko@3shape.com>
2024-02-23 15:20:58 +0200

    Add Storybook and Button component stories

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,13 +1,35 @@
-import { styled } from 'styled-components'
+import { styled, css } from 'styled-components'
+import { ButtonVariant } from './Button'
 
-export const BaseButton = styled.button`
-    padding: 1rem;
-    color: #fff;
-    background-color: #d1004b;
-    text-transform: uppercase;
-    border: none;
+type ButtonProps = {
+    $variant?: ButtonVariant;
+};
 
-    &:hover {
+const buttonVariantMap: Record<ButtonVariant, any> = {
+    solid: css<ButtonProps>`
+      border: none;
+      color: #fff;
+      background-color: #d1004b;
+
+      &:hover {
         background-color: #b6163e;
-    }
+      }
+    `,
+    outlined: css<ButtonProps>`
+      border: 1px solid #d1004b;
+      color: #d1004b;
+      background-color: transparent;
+
+      &:hover {
+        color: #fff;
+        background-color: #b6163e;
+      }
+    `,
+  };
+
+export const BaseButton = styled.button<ButtonProps>`
+    padding: 1rem;
+    text-transform: uppercase;
+
+    ${({ $variant = 'solid' }) => buttonVariantMap[$variant]}
 `
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,18 @@
 import React, { FC, ReactNode, MouseEventHandler } from "react"
 import { BaseButton } from './Button.styles'
 
+export type ButtonVariant = 'solid' | 'outlined';
+
 interface Props{
     children: ReactNode | ReactNode[];
     onClick?: MouseEventHandler<HTMLButtonElement>;
+    variant?: ButtonVariant;
 }
 
-const Button:FC<Props> = ({children, onClick}) => {
+const Button:FC<Props> = ({children, onClick, variant}) => {
   return (
     <BaseButton 
+      $variant={variant}
       onClick={onClick}
     >{children}</BaseButton>
   )
